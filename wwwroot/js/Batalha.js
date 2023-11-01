0fde1154a4e5664e2dfeeb8344ece99719693dbc
amarallandre <111255294+amarallandre@users.noreply.github.com>
2023-11-01 14:03:31 -0300

    adicionado botao de batalha, e as informações do personagem dentro do PopUp

---
+++ wwwroot/js/Batalha.js
@@ -0,0 +1,31 @@
+function openPopup() {
+    document.getElementById("batalha").style.display = "block";
+}
+
+// Função para fechar o pop-up
+function closePopup() {
+    document.getElementById("batalha").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Event listener para o botão de abrir pop-up
+    document.getElementById("abrirbatalha").addEventListener("click", openPopup);
+});
+
+// Função para realizar um ataque
+function realizarAtaque() {
+    // Simulação: Reduzir HP e MP ao realizar um ataque
+    var hpAtual = parseInt(document.getElementById("hpValue").textContent);
+    var mpAtual = parseInt(document.getElementById("mpValue").textContent);
+
+    // Reduz 10 de HP e 5 de MP (valores arbitrários)
+    hpAtual -= 10;
+    mpAtual -= 5;
+
+    // Atualiza os valores exibidos
+    document.getElementById("hpValue").textContent = Math.max(0, hpAtual);
+    document.getElementById("mpValue").textContent = Math.max(0, mpAtual);
+
+    // Exibe mensagem no console (substitua isso pela lógica real do seu jogo)
+    console.log("Ataque realizado! HP e MP reduzidos.");
+}
